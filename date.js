e351f3b790d2a0612dd65e85e3826d1477e3e557
tatsiana <Tatsiana3345260@gmail.com>
2023-01-11 18:19:23 +0300

    не готово, не понятно

--- date.js
+++ date.js
@@ -1,58 +1,34 @@
 
-// let now = new Date();
-
-// console.log(now);
-
-// function formatDate(date){
-//     if(now.getDate > )
-
-// }
-
-
-
 function formatDate(date) {
-    let difference = new Date() - date;
-    if (difference < 1000) {
-      return "прямо сейчас";
+  let now = new Date() - date;
+      // year = now.getFullYear();
+      // month = now.getMonth();
+      // date = now.getDate();
+      // hours = now.getHours();
+      // minutes = now.getMinutes();
+      // seconds = now.getSeconds();
+
+    if(now < 1000) {
+      return "\"прямо сейчас\"";
     }
-    let seconds = Math.floor(difference / 1000);
+ 
+    seconds = Math.floor(now / 1000);
+
     if (seconds < 60) {
-      return seconds + " сек.назад";
+      return seconds + "\"секунд назад\""
+    } 
+
+     minutes = Math.floor(now / 60000);
+    if (minutes < 60) {
+        return minutes + "\"минут назад\"";
     }
-    let min = Math.floor(seconds / 60);
-    if (min < 60) {
-      return min + " мин.назад";
-    }
-    let day = date.getDate();
-    let month = date.getMonth() + 1;
-    let year = date.getFullYear();
-    let hours = date.getHours();
-    let minutes = date.getMinutes();
-    if (day < 10) {
-      day = "0" + day;
-    }
-    if (month < 10) {
-      month = "0" + month;
-    }
-    if (hours < 10) {
-      hours = "0" + hours;
-    }
-    if (minutes < 10) {
-      minutes = "0" + minutes;
-    }
-    return `${day}.${month}.${year} ${hours}:${minutes}`;
-  }
+
+ }
+ console.log(formatDate(new Date(new Date - 1)));
+
+ console.log(formatDate(new Date(new Date - 30 * 1000)));
+ 
+ console.log(formatDate(new Date(new Date - 5 * 60 * 1000)));
+ 
 
 
-
-
-
-
-// console.log( formatDate(new Date(new Date - 1)) ); // "прямо сейчас"
-
-// console.log( formatDate(new Date(new Date - 30 * 1000)) ); // "30 сек. назад"
-
-// console.log( formatDate(new Date(new Date - 5 * 60 * 1000)) ); // "5 мин. назад"
-
-// // вчерашняя дата вроде 31.12.2016, 20:00
-// console.log( formatDate(new Date(new Date - 86400* 4 * 1000)) );
